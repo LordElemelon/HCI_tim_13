42cf042ede12cf22acf47b1db2f14cb804d0b22b
Ancicii <ana.anchy.pjevic7@gmail.com>
2019-04-06 17:20:26 +0200

    Ignjatovic :D

---
+++ tim_13_forecast/tim_13_forecast/ComplexWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tim_13_forecast
+{
+    /// <summary>
+    /// Interaction logic for ComplexWindow.xaml
+    /// </summary>
+    public partial class ComplexWindow : Window
+    {
+        public ComplexWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
